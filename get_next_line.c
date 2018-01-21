896f4e62ebd5d8d8b30a7ce55b8b22a9f5dc8924
cmiran <cmiran@student.42.fr>
2018-01-21 19:35:30 +0100

    rev

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,18 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 14:50:48 by cmiran            #+#    #+#             */
-/*   Updated: 2018/01/19 05:53:05 by cmiran           ###   ########.fr       */
+/*   Updated: 2018/01/19 13:12:21 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"	
+
+void		*del(char *str, size_t n)
+{
+	ft_bzero(str, n + 1);
+	free(str);
+	return (0);
+}
 
 t_list	*get_fd(t_list **list, const int fd)
 {
@@ -23,7 +30,8 @@ t_list	*get_fd(t_list **list, const int fd)
 			break;
 		file = file->next;
 	}
-	file = ft_lstnew("\0", BUFF_SIZE);
+	if (!(file = ft_lstnew("\0", BUFF_SIZE)))
+		return (0);
 	ft_lstadd(list, file);
 	file = *list;
 	file->content_size = fd;
@@ -40,6 +48,9 @@ int 		get_next_line(const int fd, char **line)
 	if (BUFF_SIZE < 1 || fd < 0 || line == NULL || read(fd, buf, 0) < 0
 			|| (!(curr = get_fd(&list, fd))))
 		return (-1);
+	if (!(*line = ft_strnew(sizeof(char))))
+		return (-1);
+	ft_print_memory(line, sizeof(t_list));
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		if (ft_strchr(buf, '\n'))
@@ -49,7 +60,9 @@ int 		get_next_line(const int fd, char **line)
 		}
 		curr->content = ft_strjoin(curr->content, buf);
 	}
-	printf("%s\n", curr->content);
-	ft_strcpy(*line, curr->content);
+	ft_strcpy((char *)line, curr->content);
+	ft_print_memory(line, sizeof(t_list));
+	printf("%s\n", (char *)line);
+	ft_lstdel(&curr, del(curr->content, BUFF_SIZE));
 	return (1);
 }

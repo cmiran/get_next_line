e1092bcb0796e6aa2cfaca64c100f43543acf119
cmiran <cmiran@student.42.fr>
2018-01-22 19:24:40 +0100

    get_next_line2.c

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 17:37:08 by cmiran            #+#    #+#             */
-/*   Updated: 2018/01/21 20:16:16 by cmiran           ###   ########.fr       */
+/*   Updated: 2018/01/22 19:02:02 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 # define GET_NEXT_LINE_H
 
 # include <fcntl.h>
-# include <unistd.h>
+# include <limits.h>
 # include <stdlib.h>
+# include <unistd.h>
 # include "../libft/libft.h"
 
 # define BUFF_SIZE 420
---
+++ get_next_line2.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/22 18:55:01 by cmiran            #+#    #+#             */
+/*   Updated: 2018/01/22 19:16:16 by cmiran           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int 		get_next_line(const int fd, char **line)
+{
+	int					ret;
+	static char	list[OPEN_MAX][sizeof(char)];
+	char				buf[BUFF_SIZE + 1];
+
+	if (BUFF_SIZE < 1 || fd < 0 || line == NULL || read(fd, buf, 0) < 0
+			|| !(*line = ft_strnew(sizeof(char))))
+		return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		if (ft_strchr(buf, '\n'))
+		{
+			ft_strcjoin(list[fd], buf, '\n');
+			break;
+		}
+		ft_strjoin(list[fd], buf);
+	}
+	ft_putendl(list[fd]);
+	ft_strcpy(*line, list[fd]);
+	return (1);
+}
